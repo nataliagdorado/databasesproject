4afc1e8e561738240be1f38365caf787608b9619
SarahDebGodano <sarah.debgodano@usp.ceu.es>
2025-05-01 09:57:14 +0200

    Tipos de Accesory
    Tambien implementé la clase Machine que la había borrado sin querer

--- src/Enums/AccessoryType.java
+++ src/Enums/AccessoryType.java
@@ -1,5 +1,14 @@
 package Enums;
 
 public enum AccessoryType {
-    //TODO fill the different types
+   BATTERY, CONNECTORS, MONITOR, POWERSUPPLY, BULB, SENSORS, TRANSDUCTORES, FILTERS  
+   
+   //Battery: portable defibrillators, vital signs monitors, or portable infusion pumps to keep them operational without a network connection.
+   //Connector: Connectors for ECG leads, or connectors in infusion and suction systems.
+   //Monitor: Vital signs monitor (measures ECG, SpO₂, blood pressure, etc.) in intensive care units (ICUs) or operating rooms.
+   //Power Supply: Power supply for neonatal incubators, portable X-ray equipment, or mechanical ventilators.
+   //Bulb: LED bulbs for surgical lights, otoscopes, endoscopes, or clinical microscopes.
+   //Sensors: Oxygen (SpO₂) sensor in pulse oximeters, temperature sensor in incubators or monitors, or flow sensor in fans.
+   //Transductors: Ultrasound transducer on ultrasound machines, or pressure transducers on invasive blood pressure monitors or arterial catheters.
+   //Filters: Bacterial filters in mechanical ventilators, HEPA filters in anesthesia equipment or laminar flow cabinets.
 }
--- src/POJOS/Machine.java
+++ src/POJOS/Machine.java
@@ -6,4 +6,72 @@ import java.util.TreeSet;
 
 public class Machine implements Serializable{
 	
+	private Integer machineId;
+    private String machineName;
+    private String machineLocation;
+    private String machineStatus;
+    private LocalDate machineInstallingDate;
+
+    public Machine(Integer machineId, String machineName, String machineLocation, String machineStatus, LocalDate machineInstallingDate) {
+        this.machineId = machineId;
+        this.machineName = machineName;
+        this.machineLocation = machineLocation;
+        this.machineStatus = machineStatus;
+        this.machineInstallingDate = machineInstallingDate;
+    }
+
+    public int getMachineId() {
+        return machineId;
+    }
+
+    public String getMachineName() {
+        return machineName;
+    }
+
+    public String getMachineLocation() {
+        return machineLocation;
+    }
+
+    public String getMachineStatus() {
+        return machineStatus;
+    }
+
+    public LocalDate getMachineInstallingDate() {
+        return machineInstallingDate;
+    }
+
+    public void setMachineInstallingDate(LocalDate machineInstallingDate) {
+        this.machineInstallingDate = machineInstallingDate;
+    }
+
+    public void setMachineId(int machineId) {
+        this.machineId = machineId;
+    }
+
+    public void setMachineName(String machineName) {
+        this.machineName = machineName;
+    }
+
+
+    public void setMachineLocation(String machineLocation) {
+        this.machineLocation = machineLocation;
+    }
+
+    public void setMachineStatus(String machineStatus) {
+        this.machineStatus = machineStatus;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Machine machines = (Machine) o;
+        return Objects.equals(machineId, machines.machineId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(machineId);
+    }
+	
 }
